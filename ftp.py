6ee73038ec4de8dfe7f17d2ecb08b7909aecd7ed
FabianMuli <fabian.muli@students.jkuat.ack.ke>
2018-08-22 15:23:39 +0300

    cannot retrieve other files - branch downloading

--- ftp.py
+++ ftp.py
@@ -9,8 +9,6 @@ try:
     ftp.login(user="fabdesig", passwd="d0ZoAm797a")
     print(ftp.getwelcome())  # welcome messages
 
-    ftp.cwd('public_html')  # changing the directory
-
     wdir = ftp.pwd()
     print(wdir)
 
@@ -19,15 +17,18 @@ try:
     for file in files:
         print(file)  # print the files in the working directory
 
-    file_copy = 'index.html'
+    file_copy = "robots"
+    file_orig = "/public_html/"
     with open(file_copy, 'w') as fp:
 
-            res = ftp.retrlines('RETR ' + wdir, fp.write)
+            res = ftp.retrlines('RETR ' + file_orig, fp.write)
 
             if not res.startswith('226 Transfer complete'):
-
                 print('Download failed')
                 if os.path.isfile(file_copy):
                     os.remove(file_copy)
 except ftplib.all_errors as e:
     print('FTP error:', e)
+
+    if os.path.isfile(file_copy):
+            os.remove(file_copy)
